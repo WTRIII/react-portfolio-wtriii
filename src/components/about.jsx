3ac0e1e67474ce000ae74b80a8f9476b46bb7a1b
WTRIII <thomren3@gmail.com>
2022-01-12 13:18:32 -0600

    added about section content

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -26,18 +26,21 @@ class About extends React.Component {
         {
           id: "first-p-about",
           content:
-            "I am an full stack web developer that has just started my journey of coding. I graduated from the University of Texas at Austin Coding Bootcamp in December of 2020. I graduated from Virginia Military Institute and began a career in the U.S. Army. I became an Armor Officer and served in Fort Benning with a Cavalry unit. After serving my time for my country, I decided to serve my community and moved back to Austin. Here, I started working for a non-profit organization in the medical industry and continued there through the worst of the COVID-19 pandemic."
+            "I am an full stack web developer that has just started my journey of coding. I graduated from the University of Texas at Austin Coding Bootcamp in December of 2020. I graduated from Virginia Military Institute and began a career in the U.S. Army. I became an Armor Officer and served in Fort Benning with a Cavalry unit. After serving my time for my country, I decided to serve my community and moved back to Austin. Here, I started working for a non-profit organization in the medical industry and continued there through the worst of the COVID-19 pandemic. Now, I feel my calling is in tech and so I decided to learn coding. Full stack web development is just the first step in my journey to being in the tech world. When I am not coding or taking care of my cats, you can find me in the gym, moving that weight. I also like to read, socialize with my friends, and chow down on a delicious spaghetti."
         },
         {
           id: "second-p-about",
           content:
-            "Now, I feel my calling is in tech and so I decided to learn coding. Full stack web development is just the first step in my journey to being in the tech world."
+            "In addition to being an entry level web developer, I am also an accomplished project manager. I managed a variety of projects in the Army as well as in the medical industry and am familiar with most corporate concepts. Something else that sets me apart from other devs is my ability to talk clearly to anyone. In my past, I needed to talk to and work with people from all walks of life - from Generals to high school graduates, from locals to foreigners. As such, I am comfortable interacting with most people and can publically speak quite well."
         },
         {
           id: "third-p-about",
           content:
-            "When I am not coding or taking care of my cats, you can find me in the gym, moving that weight. I also like to read, socialize with my friends, and chow down on a delicious spaghetti."
+            "In terms of languages and skills, I learned the following from the coding bootcamp: React, MERN, MongoDB, JavaScript, MySQL, Jquery, Handlebars, Bootstrap, HTML, CSS."
+            
         }
+
+        
       ]
     };
   }
